bea3841ec732c8ea6905549317a67c724d4af1bf
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-22 10:31:19 -0500

    progress

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,8 +1,10 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -10,16 +12,21 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+    public String processAddUserForm(Model model, @ModelAttribute @Valid User user, String verify, Errors errors) {
+//        model.addAttribute("user", user);
+//        model.addAttribute("verify", verify);
+//        model.addAttribute("username", user.getUsername());
+//        model.addAttribute("email", user.getEmail());
+        if (errors.hasErrors()) {
+            model.addAttribute("errorMsg", "big error business");
+            return "user/add";
+        }
         if (user.getPassword().equals(verify)) {
             return "user/index";
         }
@@ -27,6 +34,7 @@ public class UserController {
             model.addAttribute("error", "Passwords do not match");
             return "user/add";
         }
+
 
     }
 
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class User {
+
+    @Size(min = 5, max = 15)
+    @NotBlank
     private String username;
+
+    @Email
     private String email;
+
+    @Size(min = 6)
+    @NotBlank
     private String password;
 
     public User() {
